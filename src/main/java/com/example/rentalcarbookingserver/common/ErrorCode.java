14ac89632bd00c7bad7f8d6f307ff72c432add9b
Cinkle <m17866622833@163.com>
2022-11-14 23:30:05 +0800

    develop:将错误描述改为英文

--- src/main/java/com/example/rentalcarbookingserver/common/ErrorCode.java
+++ src/main/java/com/example/rentalcarbookingserver/common/ErrorCode.java
@@ -5,13 +5,13 @@ package com.example.rentalcarbookingserver.common;
  */
 public enum ErrorCode {
 
-    INTERNAL_ERROR(2100, "服务器内部错误"),
-    DATE_FORMAT_ERROR(2101, "未输入日期或日期格式不正确，正确的格式为（yyyy-MM-dd）"),
-    DATE_CONFLICT_ERROR(2102, "预定时间段冲突"),
-    NO_CAR_MODEL_ERROR(2103, "没有输入型号或没有该型号的车辆"),
-    NO_CAR_STOCK_ERROR(2104, "该型号的车辆无库存"),
-    DATE_COMPARE_ERROR(2105, "结束日期必须等于或大于开始日期"),
-    PARSE_JSON_ERROR(2106, "无法解析JSON参数");
+    INTERNAL_ERROR(2100, "Server internal error"),
+    DATE_FORMAT_ERROR(2101, "Date not entered or the date format is incorrect, the correct format is(yyyy-MM-dd)"),
+    DATE_CONFLICT_ERROR(2102, "Scheduled time slots conflict"),
+    NO_CAR_MODEL_ERROR(2103, "No model number entered or no car for that model"),
+    NO_CAR_STOCK_ERROR(2104, "This model of car is not in stock"),
+    DATE_COMPARE_ERROR(2105, "The end date must be equal to or greater than the start date"),
+    PARSE_JSON_ERROR(2106, "The JSON could not be parsed");
 
     private int code;
     private String message;
